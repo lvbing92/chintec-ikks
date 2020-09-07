d9671c79099c3b27df7b191523fc06379176f6d1
lvbing92 <lvbing1992>
2020-09-07 17:51:22 +0800

    init project

---
+++ ikks-erp/src/main/java/com/chintec/ikks/erp/IkksErpApplication.java
@@ -0,0 +1,16 @@
+package com.chintec.ikks.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author rubin
+ * @version 1.0
+ * @date 2020/9/7 17:24
+ */
+@SpringBootApplication
+public class IkksErpApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IkksErpApplication.class,args);
+    }
+}
