1a0da5957a9f6d6e7be3addd05c29d27a8ff0765
lvbing92 <lvbing1992>
2020-09-11 09:12:25 +0800

    init project

---
+++ ikks-generator/src/main/java/com/chintec/ikks/generator/MyGenerator.java
@@ -0,0 +1,15 @@
+package com.chintec.ikks.generator;
+
+
+import com.chintec.ikks.generator.config.GeneratorConfig;
+
+/**
+ * @author Jeff·Tang
+ * @version 1.0
+ * @date 2020/6/17 9:43
+ */
+public class MyGenerator {
+    public static void main(String[] args) {
+    GeneratorConfig.getMyGenerator("TDepartment","t_department","rubIn·lv");
+    }
+}
---
+++ ikks-generator/src/main/java/com/chintec/ikks/generator/config/GeneratorConfig.java
@@ -0,0 +1,101 @@
+package com.chintec.ikks.generator.config;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Jeff·Tang
+ * @version 1.0
+ * @date 2020/6/17 11:04
+ */
+public class GeneratorConfig {
+    public static void getMyGenerator(String modules, String tableName, String author) {
+        AutoGenerator mpg = new AutoGenerator();
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        String projectPath = System.getProperty("user.dir");
+        gc.setOutputDir(projectPath + "/mybatis-generator/src/main/java")
+                //是否文件覆盖
+                .setFileOverride(true)
+                //作者
+                .setAuthor(author)
+                .setOpen(false)
+                //主策略
+                .setIdType(IdType.AUTO)
+                .setBaseColumnList(true)
+                .setBaseResultMap(true)
+                //是否支持vr模式
+                .setActiveRecord(true);
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://182.192.165.228:3306/chintec?useUnicode=true&characterEncoding=UTF-8");
+        dsc.setSchemaName("public");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("root");
+        dsc.setPassword("root");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setParent("com.chintec.auth");
+        mpg.setPackageInfo(pc);
+        pc.setModuleName(modules);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+
+        // 如果模板引擎是 freemarker
+        String templatePath = "/templates/mapper.xml.ftl";
+        // 如果模板引擎是 velocity
+        // String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/mybatis-generator/src/main/resources/mapper/"
+                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+        templateConfig.setXml(null);
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        strategy.setInclude(tableName.split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+}
