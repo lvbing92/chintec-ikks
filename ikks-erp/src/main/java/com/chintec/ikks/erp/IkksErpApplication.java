01558730150fe75f00c08cfcfc87918ec06ab4b5
tangxinli <jeff.li@chintec.net>
2020-09-11 14:03:02 +0800

    init project

--- ikks-erp/src/main/java/com/chintec/ikks/erp/IkksErpApplication.java
+++ ikks-erp/src/main/java/com/chintec/ikks/erp/IkksErpApplication.java
@@ -1,7 +1,10 @@
 package com.chintec.ikks.erp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author rubin
@@ -9,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/9/7 17:24
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+@MapperScan("com.chintec.ikks.erp.mapper")
 public class IkksErpApplication {
     public static void main(String[] args) {
         SpringApplication.run(IkksErpApplication.class,args);
---
+++ ikks-erp/src/main/java/com/chintec/ikks/erp/conifg/selfauthor/WebSecurityConfig.java
@@ -0,0 +1,69 @@
+package com.chintec.ikks.erp.conifg.selfauthor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author rubin
+ * @version 1.0
+ * @date 2020/8/25 14:26
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /**
+     * 密码编码验证器
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    /**
+     * 自定义UserDetailsService用来从数据库中根据用户名查询用户信息以及角色信息
+     */
+
+    @Autowired
+    @Qualifier("userDetailsServiceImpl")
+    public UserDetailsService userDetailsService;
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    /**
+     * 验证配置
+     * @param http
+     * @throws Exception
+     */
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/**").authenticated()
+                .and()
+                .userDetailsService(userDetailsService);
+    }
+
+}
